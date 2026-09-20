a2653bb2c29fca7bfcedee6fce3dd515edbecb19
N C <noog6@users.noreply.github.com>
2026-09-20 12:33:41 -0400

    Harden Job continuation attention acceptance

--- embodied_runtime/jobs/__init__.py
+++ embodied_runtime/jobs/__init__.py
@@ -2,9 +2,13 @@
 
 from .model import InvalidJobRunTransitionError, Job, JobRun, JobRunStatus, JobTarget
 from .execution import JobWorkDisposition, JobWorkOutcome
+from .continuation import (
+    JobContinuation, JobContinuationController, JobContinuationState,
+)
 from .sqlite_store import SQLiteJobStore
 from .store import JobStore
 
 __all__ = ["InvalidJobRunTransitionError", "Job", "JobRun", "JobRunStatus",
            "JobStore", "JobTarget", "JobWorkDisposition", "JobWorkOutcome",
-           "SQLiteJobStore"]
+           "SQLiteJobStore", "JobContinuation", "JobContinuationController",
+           "JobContinuationState"]
